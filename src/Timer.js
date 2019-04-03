5285e93104bc1eb258856b1c1c787075969fe762
Emily Wagner <EmilyWagner598@gmail.com>
2019-04-03 10:24:14 -0700

    Done

--- src/Timer.js
+++ src/Timer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+ import React, { Component } from 'react';
 
 class Timer extends Component {
 
@@ -7,11 +7,13 @@ class Timer extends Component {
     color: '#'+Math.floor(Math.random()*16777215).toString(16)
   }
 
-  // add your code here
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000);
+  }
 
-
-
-
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
 
 
